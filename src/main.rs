cd68fd126fd425ddd03c91486d0cc37ec0df9490
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-25 17:07:10 +0200

    feat: file reading and requesting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+async fn get(url: &str) -> std::result::Result<String, reqwest::Error> {
+    let client = reqwest::Client::new();
+
+    let response = client.get(url).send().await?;
+
+    response.text().await
+}
+
 fn main() {
-    println!("Hello, world!");
+    let seed_file = File::open("seeds.txt").expect("no seeds.txt found");
+    let reader = BufReader::new(seed_file);
+
+    let seeds: Vec<String> = reader
+        .lines()
+        .collect::<Result<_, _>>()
+        .expect("failed to parse seeds.txt");
+
+    for line in &seeds {
+        println!("{}", line);
+    }
 }
